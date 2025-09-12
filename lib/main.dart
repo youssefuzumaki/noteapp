89d385b0da6f916a96c9688e1de75431e3d81105
youssefuzumaki <yy999555yy@gmail.com>
2025-09-12 14:27:13 +0300

    Initial commit - noteapp

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:noteapp/Screens/Home_Page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+
+      home: const HomePage(),
+    );
+  }
+}
